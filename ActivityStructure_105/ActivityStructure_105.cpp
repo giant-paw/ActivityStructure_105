f081f24f811aa99976e4c37f18242cabdc9e39ec
giant-paw <giantpawbusiness@gmail.com>
2023-04-03 03:18:48 +0700

    pembuatan struct mahasiswa

--- ActivityStructure_105/ActivityStructure_105.cpp
+++ ActivityStructure_105/ActivityStructure_105.cpp
@@ -1,20 +1,22 @@
-// ActivityStructure_105.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+struct mahasiswa {
+	string NIM;
+	string nama;
+	string alamat;
+	int umur;
+};
+
+int main() {
+	mahasiswa mhs1, mhs2;
+
+	mhs1.NIM = "20220140105";
+	mhs1.nama = "Giant";
+	mhs1.alamat = "Klaten";
+	mhs1.umur = 19;
+
+	cout << "Masukkan NIM : ";
+	cin >> mhs1.NIM;
+
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
