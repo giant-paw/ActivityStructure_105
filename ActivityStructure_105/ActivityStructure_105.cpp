a55608b388fe4228412c1e16407ae55cb443fa43
giant-paw <giantpawbusiness@gmail.com>
2023-04-03 03:21:19 +0700

    inputan atribut dari mhs2

--- ActivityStructure_105/ActivityStructure_105.cpp
+++ ActivityStructure_105/ActivityStructure_105.cpp
@@ -18,5 +18,10 @@ int main() {
 
 	cout << "Masukkan NIM : ";
 	cin >> mhs1.NIM;
-
+	cout << "Masukkan Nama : ";
+	cin >> mhs1.nama;
+	cout << "Masukkan Alamat : ";
+	cin >> mhs1.alamat;
+	cout << "Masukkan Umur : ";
+	cin >> mhs1.umur;
 }
